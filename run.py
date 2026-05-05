82c010b44cd9a14d127008c5f3bf4723f6b2a158
ryan <mr.yaryyev@gmail.com>
2026-05-05 18:50:53 +0900

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -6,5 +6,34 @@
 
 
 print("Hello World!")
+print("PYTHON: Everything is object!")
+#hem qiymati hamda reference bor
 
-# python3 run.py
+message = "PYTHON: Everything is object!"
+print(message)
+
+result = type(message)
+print("data type:", result)
+
+# print,type => builtin functionlar
+
+'''
+dunder degan narosmi tushuncha bor
+Dunder = __ = double underscore
+
+Pythonni ihslayshi DUNDER VARIABLELAR yoki MAHSUS(magic) METOTLARGA asoslanadi.
+
+__builtins__ => pythonni system variablelari hisoplanadi
+__init__ => pythonni magic methodi
+'''
+
+''' In Python, there are builtin (ichki ornatilgan) tools:
+(1) TYPES > int float str list dict
+(2) FUNCTION > print(), len(), input(), type()
+(3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))
+
+
+# python3 run.py   
