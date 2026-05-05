8f985ae59887ab373f6b92845bb99618e5061146
ryan <mr.yaryyev@gmail.com>
2026-05-05 21:19:26 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -10,3 +10,21 @@ print(f"the count: {count}, and type: {count_type}") # format string
 result1 = count.bit_count() # method [cube]()// OBJECTLAR => uzining hir hator propertylarga (method hamda  statelar) ega bolgan mahsus data type.
 result2 = count.numerator # state [wrench-achor]
 print(result1, result2)
+
+print("==== string ====")
+ # METHODS: uppper() lower() title() find() replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "MasterClass")
+print(f"the result (4): {result}")
+
+print(course) # AI Python Fullstack => ozgarmaydi
