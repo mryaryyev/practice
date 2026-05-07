b158cd4a7791ed71952d2b36388e0043baa7d7bf
ryan <mr.yaryyev@gmail.com>
2026-05-07 16:20:42 +0900

    fix: learn keyword & default arguments

--- functions.py
+++ functions.py
@@ -12,11 +12,11 @@ print("==== DEFINE (parametr) vs CALL (argument) ====")
 
 
 # DEFINE - build / parametr
-def greet(a): # VOID Function
-    print(f"How do you do, {a}") 
+def greet(a):  # VOID Function
+    print(f"How do you do, {a}")
 
 
-def greeting(b): # RETURN Function
+def greeting(b):  # RETURN Function
     print("greeting is executed")
     return f"Hi {b}"
 
@@ -32,3 +32,21 @@ print("result1: ", result1)
 
 result2 = greeting("Justin")
 print("result2: ", result2)
+
+
+print("==== Keyword & default arguments ====")
+
+# DEFINE
+
+
+def give_greet(name, age=28):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old!"
+
+# CALL
+# result3 = give_greet("Ryan", 34)
+result3 = give_greet(name="Ryan", age=34) # keyword => name, age
+print("result3: ", result3)
+
+result4 = give_greet("John")
+print("result4: ", result4)
