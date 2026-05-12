f10e62cb19f1da84bd8601839fa64a91f0693eb0
ryan <mr.yaryyev@gmail.com>
2026-05-12 16:02:34 +0900

    feat: learn conditions and logical operators

--- opercon.py
+++ opercon.py
@@ -37,3 +37,45 @@ print(id(c), id(d))
 
 print("c is d", c is d) # False ==> different reference
 print("c is e", c is e) # True ==> same reference and value
+
+
+print(" ===== Condition =====")
+x = 5
+
+if x > 50:
+    print("Case A")
+elif x > 10:
+    print("Case B")
+else:
+    print("Case C")
+
+print(" ===== Logical Operators =====")
+age = 20
+# person = None
+
+# if age > 18:
+#     person = "adult"
+# else:
+#     person = "child"
+
+# print("person:", person)
+
+# Ternary = logical
+person = "adult" if age > 18 else "minor"
+print("person:", person)
+
+print("-"*5)
+
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = False
+
+if not is_student:
+    print("Welcome here, do you want to be student")
+elif is_admin:
+    print("Please go to this office!")
+elif is_guest or is_parent:
+    print("Waiting room is over there!")
+else:
+    print("Other cases")
