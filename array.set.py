de0193d6331b6004bcc1e4cd34f58a6e6c875a33
ryan <mr.yaryyev@gmail.com>
2026-05-15 10:48:41 +0900

    fix: specific operators with set

--- array.set.py
+++ array.set.py
@@ -24,3 +24,40 @@ print("numbers(3):", numbers)
 
 del numbers[0:2]
 print("numbers(4):", numbers)
+
+
+print("===== Set =====")
+
+# { } takrorlamaydigan kolleksiya
+# Set of unique collection without keeping order!
+
+new_numbers = array("i", [1, 4, 7, 5, 4, 5, 7, 8, 41])
+numbs_set = set(new_numbers)
+
+print(f"numbs_set: {numbs_set} and type: {type(numbs_set)}")
+
+numbs_set.add(200)
+print("numbs_set(1):", numbs_set)
+
+numbs_set.add(7)
+print("numbs_set(2):", numbs_set)
+
+
+print("===== Specific operators =====")
+
+# | & - ^
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b # union
+print("result1:", result1)
+
+result2 = a & b # intersection
+print("result2:", result2)
+
+result3 = a - b # difference
+print("result3:", result3)
+
+result4 = a ^ b # symmetric difference
+print("result4:", result4)
